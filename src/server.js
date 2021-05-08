bce53f4e184ad93300e71508c56e44a8b36ee20d
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2021-05-08 16:53:39 -0400

    added support mongoDB

--- src/server.js
+++ src/server.js
@@ -1,10 +1,34 @@
 const express = require("express");
+// const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
 
-app.use(cors);
+// mongo db: add mongoose
+// mongoose.connect(`mongodb+srv://`, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
+
+app.use(cors());
+const allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
 app.use(express.json());
 app.use(routes);
 
